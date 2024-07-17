1e943ce172f2c540977757e8640e310ec827e3aa
saqlainmusi <rakibshaharier96@gmail.com>
2024-07-17 19:29:15 +0600

    project upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# vue-joblist
+Job Listing Project
---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Navbar from '../src/components/Navbar.vue';
+import Hero from '../src/components/Hero.vue';
+import Homecards from '../src/components/HomeCards.vue';
+import JobListings from '../src/components/JobListings.vue';
+</script>
+
+<template>
+  <div>
+ <Navbar />
+ <Hero title="Jobs For Frontend Dev" subtitle="Find the Vue job that fits your skills and needs"/>
+ <Homecards/>
+ <JobListings :limit="3" :showButton="true"/>
+</div>
+</template>
